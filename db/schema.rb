9e62d48f2866fce6ce70d834ef0561e11054a8c1
Eric Roos <ericroos@hey.com>
2023-04-16 14:33:25 -0500

    omniauth via devise for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_16_183017) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_190945) do
   create_table "projects", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_183017) do
     t.string "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["uuid"], name: "index_users_on_uuid", unique: true
   end
 
