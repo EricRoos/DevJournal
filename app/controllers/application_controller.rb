40b7cd130d73876776cef4d321930c3ab4cb1350
Eric Roos <ericroos@hey.com>
2023-04-17 15:31:01 -0500

    cache current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   before_action :authenticate_user!, unless: -> { devise_controller? }
-  before_action :log_current_user
   after_action :verify_authorized, unless: -> { devise_controller? }
 
-  def log_current_user
-    Rails.logger.info("Current User: #{current_user}")
+  def current_user
+    warden_key = session["warden.user.user.key"].first.first
+    session_id = session["session_id"]
+    @current_user ||= Rails.cache.fetch([warden_key, session_id]) do
+      User.cached_find(warden_key)
+    end
+  rescue
+    super
   end
+
 
   def authenticate_user!
     return if current_user.present?
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :registerable, :omniauthable, omniauth_providers: [:github]
 
+  def self.find(id, use_cached_version: true)
+    if use_cached_version
+      cached_find(id)
+    else
+      super
+    end
+  end
+
+  def update_cache_keys
+    Rails.cache.write([Project, :cache_key, self.id], self.updated_at)
+  end
+
+  def self.cached_find(id)
+    updated_at = Rails.cache.read([Project, :cache_key, id])
+    project = nil
+
+    if !updated_at
+      project = Project.find(id)
+      updated_at = project.updated_at
+      project.update_cache_keys
+    end
+
+    Rails.cache.fetch([Project, :cached, id, updated_at]) do
+      project ||= Project.find(id)
+    end
+  end
   def self.from_omniauth(auth)
     find_or_create_by(source: auth.provider, uuid: auth.uid) do |user|
       user.email = auth.info.email
