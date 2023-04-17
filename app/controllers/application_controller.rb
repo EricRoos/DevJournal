42a78548c16e7363252da494b70c32ebfe6ab34b
Eric Roos <ericroos@hey.com>
2023-04-16 22:01:12 -0500

    layout stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { devise_controller? }
   before_action :log_current_user
   after_action :verify_authorized, unless: -> { devise_controller? }
 
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     Rails.logger.info("Current User: #{current_user}")
   end
 
+  def authenticate_user!
+    return if current_user.present?
+    redirect_to new_user_session_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get '/users/sign_in' => 'devise/sessions#new'
+    get '/users/sign_in' => 'devise/sessions#new', as: :new_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "projects#index"
 end
