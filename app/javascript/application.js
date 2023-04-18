e78ae98a910d37e8f46ce8f5bf55d7f11a944484
Eric Roos <ericroos@hey.com>
2023-04-17 22:18:34 -0500

    add editorjs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+if(document.getElementById('editorjs')){
+  const editor = new EditorJS({
+    /**
+     * Id of Element that should contain Editor instance
+     */
+    holder: 'editorjs',
+    autofocus: false,
+    tools: {
+      header: Header,
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true,
+      },
+    }
+  })
+}
+
