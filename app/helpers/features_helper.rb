19deb04a93b043b2ba325fd3f24213611e85e651
Eric Roos <ericroos@hey.com>
2023-04-19 10:39:05 -0500

    update status for feature

--- app/helpers/features_helper.rb
+++ app/helpers/features_helper.rb
@@ -1,2 +1,13 @@
 module FeaturesHelper
+  def status_color(feature)
+    if feature.in_progress?
+      "bg-orange-200 text-orange-600"
+    elsif feature.not_started?
+      "bg-gray-200 text-gray-600" 
+    elsif feature.complete?
+      "bg-green-300 text-green-700" 
+    else
+      "text-gray-800"
+    end
+  end
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,9 @@
 class Feature < ApplicationRecord
+  enum status: {
+    not_started: "not_started",
+    in_progress: "in_progress",
+    complete: "complete"
+  }
+
   belongs_to :project
 end
